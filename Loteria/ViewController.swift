0935ade172c6eb772eb793f30d0f103b52fee0ce
Cristovão <cristovao.s.lima@gmail.com>
2022-06-30 20:09:19 -0300

    Initial Commit

---
+++ Loteria/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Loteria
+//
+//  Created by Cristovão Lima on 30/06/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
